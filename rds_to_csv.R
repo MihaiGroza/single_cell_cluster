e6b127004c0d2f30531da95c42b089427e069c27
Mihai G <mihaigroza@protonmail.com>
2020-07-03 23:41:38 -0400

    implemented pca and umap

--- rds_to_csv.R
+++ rds_to_csv.R
@@ -2,7 +2,7 @@ library(SingleCellExperiment)
 df <- readRDS('/home/mihaig/Documents/single_cell_cluster/baron-human.rds')
 assayed_data <- assay(df)
 
-write.csv(assayed_data, file = gzfile("/home/mihaig/Documents/single_cell_cluster/human_pancreas_cells.csv"))
+write.csv(assayed_data, file = gzfile("/home/mihaig/Documents/single_cell_cluster/human_pancreas_cells.csv.gz"))
 
 
 
